719a129477efe3227a6ea3d30c4b70e43a0619a9
vanshita00 <129608607+vanshita00@users.noreply.github.com>
2024-04-08 18:09:28 +0530

    Add files via upload

---
+++ blogger/blogger-core/src/main/java/com/blogger/core/Main.java
@@ -0,0 +1,13 @@
+package com.blogger.core;
+
+import com.blogger.util.Utility;
+import jdk.jshell.execution.Util;
+
+import static com.blogger.util.Utility.isNull;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(Utility.isNull(null));
+    }
+}
